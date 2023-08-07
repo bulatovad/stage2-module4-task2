6e5aa59faa12e796c9dfd4fcaca5b413e0f021f7
bulatovad <damira.bulatova@gmail.com>
2023-08-07 22:48:38 +0600

    updated isClosed method

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -23,6 +23,6 @@ public class ProxyConnection implements Connection {
 
     @Override
     public boolean isClosed() {
-        return realConnection == null;
+        return realConnection.isClosed();
     }
 }
